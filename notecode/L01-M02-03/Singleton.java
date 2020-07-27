b34dfcbfadea018a0e1088a4e6d2915f094aee99
xsnaruto <xsnaruto@users.noreply.github.com>
2020-07-27 20:37:33 +0800

    update

--- notecode/L01-M02-03/Singleton.java
+++ notecode/L01-M02-03/Singleton.java
@@ -3,5 +3,5 @@
  */
 public class Singleton {
 
-    private Singleton(){};
+    private Singleton(){};  // 设置 private 使得方法无法创建对象
 }
--- notecode/L01-M02-03/SingletonTest.java
+++ notecode/L01-M02-03/SingletonTest.java
@@ -6,10 +6,10 @@ public class SingletonTest {
     public static void main(String[] args) {
 
         // 1. 声明 Singleton 类型的引用指向该类型的对象
-        Singleton s1 = new Singleton();
-        Singleton s2 = new Singleton();
+        // Singleton s1 = new Singleton();
+        // Singleton s2 = new Singleton();
 
-        System.out.println(s1 == s2); // false， 两个都是新创建的不同引用地址的空间
+        // System.out.println(s1 == s2); // false， 两个都是新创建的不同引用地址的空间
 
     }
 }

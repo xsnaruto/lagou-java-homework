40192597fcf118b7ddbb8989475045fc490b190e
xsnaruto <xsnaruto@users.noreply.github.com>
2020-07-30 16:22:42 +0800

    update

--- notecode/L01-M01-05/ArrayTest.java
+++
@@ -1,5 +0,0 @@
-public class ArrayTest {
-	public static void main(String[] args) {
-		int[] arr = new int[2];
-	}
-}
--- notecode/L01-M02-03/Singleton.java
+++
@@ -1,7 +0,0 @@
-/**
- * Singleton
- */
-public class Singleton {
-
-    private Singleton(){};  // 设置 private 使得方法无法创建对象
-}

d0693e31a30a0793b6ae018f34971d4d456e9049
xsnaruto <xsnaruto@users.noreply.github.com>
2020-08-05 23:13:50 +0800

    update

--- nadacode/src/level_01/module_02/class_04/Runner.java
+++
@@ -1,5 +0,0 @@
-package level_01.module_02.class_04;
-
-public interface Runner {
-    void run();
-}
--- nadacode/src/level_01/module_02/class_05/DirectionTest.java
+++
@@ -1,8 +0,0 @@
-package level_01.module_02.class_05;
-
-public class DirectionTest {
-    public static void main(String[] args) {
-
-
-    }
-}
